a248f7b4c168b835ad42bfa914e86365e201cc7b
ebuckley <emmanuel.buckley@eleadcrm.com>
2016-12-12 19:41:53 -0600

    Initial Commit

---
+++ src/JSS.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("C03SCCM01")]
+[assembly: AssemblyProduct("JSS.Data")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d29831e0-144f-4549-b591-b225d09614d6")]
